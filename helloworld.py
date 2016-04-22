0b308dd48f458848c159b2eddbd040505ddd6ecc
sripley52 <sripley68@gmail.com>
2016-04-22 12:27:49 -0600

    Initial tkinter
    Explain how tkinter works

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,13 @@
-print ("Hello world!")
+from tkinter import *
+
+#root = ordinary window with title bar
+root = Tk()
+
+#Label = display either text or an icon or other image
+w = Label(root, text="Hello, world!")
+#pack = tells widget to size itself to fit the given text,
+#and make itself visible
+w.pack()
+
+#make window appear
+root.mainloop()
