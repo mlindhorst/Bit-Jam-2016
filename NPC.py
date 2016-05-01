2746e267b897f350319d2d7d9377b984bde9f3d0
binjobrush <kallman.jc@gmail.com>
2016-04-30 21:34:36 -0600

    get NPC response returns none if response is not found
    get NPC response returns none if response is not found

--- NPC.py
+++ NPC.py
@@ -6,8 +6,15 @@ class NPC:
         self.door_num = door_num
         self.responses = responses
         
-    def get_response(self, question_number):
-        return self.responses[question_number]
+    def get_response(self, question):
+        if(question.isdigit()):
+            question_number = int(question)
+            if(question_number > 0 and question_number < 7):
+                return self.responses[question]
+            else:
+                return None
+        else:
+            return None
         
 class NPCCreator:
     def __init__(self):
